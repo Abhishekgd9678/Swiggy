faa25d9f98c374ee52d4848298fdd2ab0b32f70b
Abhishekgd9678 <abhishekgd2003@gmail.com>
2023-11-25 12:42:40 +0530

    Improved ux 2.0

--- Body.js
+++ Body.js
@@ -1,94 +1,88 @@
-
 import { useEffect, useState } from "react";
-import Restaurant from "./Restaurant"
-import  Shimmer from "./Shimmer"
+import Restaurant from "./Restaurant";
+import Shimmer from "./Shimmer";
 
-const Body=()=>
-{
-    const [searchtext,settext]=useState("");
-    const [res,setres]=useState([]);
-    const [err,seterr]=useState("");
-    useEffect(()=>{ 
-        fetchData();
-    },[]);
+const Body = () => {
+  const [searchtext, settext] = useState("");
+  const [filterres, setfilterres] = useState([]);
+  const [res, setres] = useState([]);
+  const [err, seterr] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
 
- 
-    
-    const fetchData = async () => {
-        try {
-            const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.28475216724439&lng=76.64010163396597&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-            const finalData = await response.json();
-           
-            const restaurants = finalData?.data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
-            if (restaurants) {
-                setres(restaurants);
-           
-            }
-        } 
-        catch (error) {
-            console.error("Error fetching data:", error);
-        }
-        seterr("")
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.28475216724439&lng=76.64010163396597&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const finalData = await response.json();
+
+      const restaurants =
+        finalData?.data.cards[2].card.card.gridElements.infoWithStyle
+          .restaurants;
+      if (restaurants) {
         
-    };
+        setres(restaurants);
+        setfilterres(restaurants);
 
-    function filter(){
-        const datanew=res.filter((x)=>x.info.avgRating>4);
-       
-  
-   
-            setres(datanew);
-        
-       
-    }
-    function unfilter()
-    {
-        fetchData();
-    
-    }
-    if(res.length===0){
-        return <Shimmer/>
-    }
-    function filterdata(){
-        let datas=res.filter((x)=>{return (x.info.name.includes(searchtext)) })
-       if(datas.length===0)
-       {
-        seterr("No matching Restaurants Found");
-       }
-       else{
         seterr("");
-        setres(datas)
-       }
-       
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-   
+  };
 
-    return (
-        <>
-            <div className="Body">
-                <div className="Search-Bar">
-                    <input type="text" onChange={(e)=>{var x=e.target.value;
-                    settext(x)}} ></input>
-                    <button onClick={filterdata}>Search</button> 
+  function filter() {
+    const datanew = res.filter((x) => x.info.avgRating > 4);
 
+    setfilterres(datanew);
+  }
+  function unfilter() {
+    setfilterres(res);
+  }
 
-                    <button onClick={filter}>Top Rated Restaurants</button>
-                    <button onClick={unfilter}>All Restaurants</button>
+  if (res.length === 0) {
+    return <Shimmer />;
+  }
 
-                </div>
-                <h4>{err}</h4>
-                    <div className="Restaurants">
-                      {res.map((x)=>{
-                        return (<Restaurant {...x} key={x.info.id}/>)
-                      })}
-             
-                        
-                        
-                </div>
-            </div>
+  function filterdata() {
+    let datas = res.filter((x) => {
+      return x.info.name.toLowerCase().includes(searchtext.toLowerCase());
+    });
+    if (datas.length === 0) {
+      seterr("No matching Restaurants Found");
+    } else {
+      seterr("");
+      setfilterres(datas);
+    }
+  }
 
-        </>
+  return (
+    <>
+      <div className="Body">
+        <div className="Search-Bar">
+          <input
+            type="text"
+            value={searchtext}
+            onChange={(e) => {
+              var x = e.target.value;
+              settext(x);
+            }}
+          ></input>
+          <button onClick={filterdata}>Search</button>
 
-    )
-}
+          <button onClick={filter}>Top Rated Restaurants</button>
+          <button onClick={unfilter}>All Restaurants</button>
+        </div>
+        <h4>{err}</h4>
+        <div className="Restaurants">
+          {filterres.map((x) => {
+            return <Restaurant {...x} key={x.info.id} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 export default Body;
